c0f4b145b7750342e34ac128d188b1e2bc61ddf4
Joe Roddy <mrjoeroddy@gmail.com>
2016-03-21 12:53:37 -0400

    fix merge conflicts

--- src/main/java/PCard/Domain/Application.java
+++ src/main/java/PCard/Domain/Application.java
@@ -1,8 +1,6 @@
 package PCard.Domain;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
